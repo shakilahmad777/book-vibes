eb2537c43015f684b1e9feb2e7a8aeaca17635a3
shakilahmad777 <shakilahmadpip3@gmail.com>
2026-02-18 07:20:35 +0600

    yahh book list added to toast message

--- src/pages/Root/Root.jsx
+++ src/pages/Root/Root.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { Outlet } from 'react-router'
 import Footer from '../../components/Footer/Footer'
 import NavBar from '../../components/Header/NavBar'
+import { Toaster } from 'react-hot-toast'
 
 export default function Root() {
   return (
     <div className='mx-5 md:mx-20 max-w-auto'>
         <NavBar></NavBar>
+        <div><Toaster position="top-right"/></div>
         <Outlet></Outlet>
         <Footer></Footer>
     </div>
--- src/utilitis/addToDB.js
+++ src/utilitis/addToDB.js
@@ -1,3 +1,4 @@
+import toast, { Toaster } from "react-hot-toast";
 const getStoredReadList = () => {
   //  stored localStorage getItem key of "read-list"    //
   const storedReadListSTR = localStorage.getItem("read-list");
@@ -13,36 +14,45 @@ const addToStoredReadDB = (id) => {
   //  stored localStorage setItem value of book ID    //
   const storedReadList = getStoredReadList();
   if (storedReadList.includes(id)) {
-    alert("this book is already exied");
+    toast.error("This book is already added");
   } else {
     storedReadList.push(id);
     const storedReadListStringify = JSON.stringify(storedReadList);
     localStorage.setItem("read-list", storedReadListStringify);
+
+    // ideally trigger toast form the component //
+    toast.success("Successfully book added to ReadList!");
   }
-};  // stored read list end //
+}; // stored read list end //
 
 const getStoredWishList = () => {
-    //  stored localStorage getItem key of "wish-list"  //
-    const storedWishListSTR = localStorage.getItem("wish-list");
-    if(storedWishListSTR) {
-        const storedWishListParse = JSON.parse(storedWishListSTR);
-        return storedWishListParse;
-    }
-    else {
-        return [];
-    }
-}
+  //  stored localStorage getItem key of "wish-list"  //
+  const storedWishListSTR = localStorage.getItem("wish-list");
+  if (storedWishListSTR) {
+    const storedWishListParse = JSON.parse(storedWishListSTR);
+    return storedWishListParse;
+  } else {
+    return [];
+  }
+};
 const addToStoredWishListDB = (id) => {
-    //  stored localStorage setItem value of book ID   //
-    const storedWishList = getStoredWishList();
-    if(storedWishList.includes(id)) {
-        alert("this book is already exied");
-    }
-    else {
-        storedWishList.push(id);
-        const storedWishListStringify = JSON.stringify(storedWishList);
-        localStorage.setItem("wish-list", storedWishListStringify);
-    }
-}   //  stored wish list end   //
+  //  stored localStorage setItem value of book ID   //
+  const storedWishList = getStoredWishList();
+  if (storedWishList.includes(id)) {
+    toast.error("This book is already added");
+  } else {
+    storedWishList.push(id);
+    const storedWishListStringify = JSON.stringify(storedWishList);
+    localStorage.setItem("wish-list", storedWishListStringify);
 
-export { getStoredReadList, getStoredWishList, addToStoredReadDB, addToStoredWishListDB };
+    // ideally trigger toast form the component //
+    toast.success("Successfully book added to WishList!");
+  }
+}; //  stored wish list end   //
+
+export {
+  getStoredReadList,
+  getStoredWishList,
+  addToStoredReadDB,
+  addToStoredWishListDB,
+};
